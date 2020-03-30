075eee863b0fbd00badadd4ab91e69da14de8bfe
unknown <deepraval2905@gmail.com>
2020-03-30 13:28:49 +0530

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/**
+ * Author RDP
+ * 
+**/
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+#include <bitset>
+#define endl '\n'
+#define bp(x) __builtin_popcount(x)
+#define zf(x) __builtin_clz(x)
+#define zl(x) __builtin_ctz(x)
+#define par(x) __bultin_parity(x)
+#define FAST_IO ios::sync_with_stdio(0); std::cin.tie(0);
+typedef long long ll;
+using namespace std;
+
+void test_case()
+{
+    FAST_IO
+    return ;
+}
+int main()
+{
+    FAST_IO
+    return 0;
+}
